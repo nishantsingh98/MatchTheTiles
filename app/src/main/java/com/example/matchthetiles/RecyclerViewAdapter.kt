7cbb205667da717a21c25828102398d1d1e5e72f
nishant <email.nishant.singh@gmail.com>
2021-01-01 13:29:31 +0530

    recycler view adapter done to do most of the work

--- app/src/main/java/com/example/matchthetiles/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/matchthetiles/RecyclerViewAdapter.kt
@@ -1,25 +1,42 @@
 package com.example.matchthetiles
 
+import android.os.Handler
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+class RecyclerViewAdapter(private var easyNumbersToFill: MutableList<Int>) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
-    inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textViewEasyItem: TextView = itemView.findViewById(R.id.textView_easy_item)
 
+        init {
+            itemView.setOnClickListener {
+                val position = adapterPosition
+                Toast.makeText(itemView.context, "position # ${position + 1}", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_view_item, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.textViewEasyItem.text = easyNumbersToFill[position].toString()
+
+        val delayMillis = 10000
+        val handler = Handler()
+        val tvRQPoint: View = holder.textViewEasyItem // your view
+        tvRQPoint.postDelayed(Runnable { tvRQPoint.visibility = View.INVISIBLE }, 3000)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return easyNumbersToFill.size
     }
 
 }
