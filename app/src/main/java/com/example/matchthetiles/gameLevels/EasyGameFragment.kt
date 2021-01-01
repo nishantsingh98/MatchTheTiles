fdc0662b82357e616b5a48562289a74796434001
nishant <email.nishant.singh@gmail.com>
2021-01-01 20:45:14 +0530

    working fine

--- app/src/main/java/com/example/matchthetiles/gameLevels/EasyGameFragment.kt
+++ app/src/main/java/com/example/matchthetiles/gameLevels/EasyGameFragment.kt
@@ -1,10 +1,12 @@
 package com.example.matchthetiles.gameLevels
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -27,11 +29,15 @@ class EasyGameFragment : Fragment(), RecyclerViewAdapter.OnItemClickListener {
         navController = Navigation.findNavController(view)
 
         val easyGameRecyclerView = view.findViewById<RecyclerView>(R.id.easyGame_recycler_view)
-        val playAgain = view.findViewById<Button>(R.id.button_play_again)
+        val playAgainButton = view.findViewById<Button>(R.id.button_easy_play_again)
+        val timerTextView = view.findViewById<TextView>(R.id.textView_easy_timer)
+
+        startTimer(timerTextView)
 
         startGame(easyGameRecyclerView)
 
-        playAgain.setOnClickListener {
+        playAgainButton.setOnClickListener {
+            startTimer(timerTextView)
             startGame(easyGameRecyclerView)
         }
     }
@@ -42,6 +48,21 @@ class EasyGameFragment : Fragment(), RecyclerViewAdapter.OnItemClickListener {
         numbers.shuffle()
         easyGameRecyclerView.layoutManager = GridLayoutManager(this.context, noOfColumns)
         easyGameRecyclerView.adapter = RecyclerViewAdapter(numbers, this)
+    }
+
+    private fun startTimer(timerTextView: TextView) {
+        var counter = 10
+        object : CountDownTimer(10000, 1001) {
+            override fun onTick(millisUntilFinished: Long) {
+                val str = "game starts in $counter"
+                timerTextView.text = str
+                counter--
+            }
+
+            override fun onFinish() {
+                timerTextView.visibility = View.INVISIBLE
+            }
+        }.start()
     }
 
     override fun onItemClick(position: Int) {
